1013a6f975b45a13b21d3079ce80f902d60933c7
orca3136driver <61524942+orca3136driver@users.noreply.github.com>
2023-01-28 16:13:20 -0500

    intake skeleton code (VERY BASIC)

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -5,9 +5,9 @@ import org.littletonrobotics.junction.AutoLog;
 public interface IntakeIO {
     @AutoLog
   public static class IntakeIOInputs {
-    public double pressure = 0.0;
+
     public boolean open = false;
-    public boolean compressor = false;
+
   }
 
 /** Updates the set of loggable inputs. */
---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSparkMax.java
@@ -0,0 +1,57 @@
+package frc.robot.subsystems.intake;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxPIDController.ArbFFUnits;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotController;
+
+public class IntakeIOSparkMax implements IntakeIO {
+    private static final double GEAR_RATIO = 1.5;
+
+    private final CANSparkMax leader;
+    private final CANSparkMax follower;
+    private final RelativeEncoder encoder;
+    private final SparkMaxPIDController pid;
+
+    public IntakeIOSparkMax() {
+        leader = new CANSparkMax(1, MotorType.kBrushless);
+        follower = new CANSparkMax(2, MotorType.kBrushless);
+
+        encoder = leader.getEncoder();
+        pid = leader.getPIDController();
+
+        leader.restoreFactoryDefaults();
+        follower.restoreFactoryDefaults();
+
+        leader.burnFlash();
+        follower.burnFlash();
+    }
+
+  private void enableVoltageCompensation() {
+    }
+
+public void setVelocity(double velocityRadPerSec, double ffVolts) {
+    pid.setReference(
+        Units.radiansPerSecondToRotationsPerMinute(velocityRadPerSec)
+            * GEAR_RATIO,
+        ControlType.kVelocity, 0, ffVolts, ArbFFUnits.kVoltage);
+  }
+
+
+  public void stop() {
+    leader.stopMotor();
+  }
+
+  public void configurePID(double kP, double kI, double kD) {
+    pid.setP(kP, 0);
+    pid.setI(kI, 0);
+    pid.setD(kD, 0);
+    pid.setFF(0, 0);
+  }
+
+}
