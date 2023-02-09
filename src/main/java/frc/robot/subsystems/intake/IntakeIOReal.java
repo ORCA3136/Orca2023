44eda581bb841f2cb27eaecd553b660ee36ef84e
lelgfnoob <jackshark656prussia@gmail.com>
2023-02-09 17:54:01 -0500

    pid intake

--- src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems.intake;
 
+import org.ejml.interfaces.decomposition.CholeskyDecomposition;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
@@ -10,33 +13,51 @@ import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxPIDController.ArbFFUnits;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Counter;
 import frc.robot.Constants.IntakeConstants;;
 
 public class IntakeIOReal implements IntakeIO {
   //  private static final double GEAR_RATIO = 1.5;
+  private final Joystick m_joystick = new Joystick(1);
     private boolean isOpen = false;
+    private static double kDt = 0.02;
+    private static double kP = 1.3;
+    private static double kI = 0.0;
+    private static double kD = 0.7;
+
+
     Counter counter;
 
     
-    private final VictorSPX chomp;
-   // private final CANSparkMax leftSide;
-  //  private final CANSparkMax rightSide;
-  //  private final RelativeEncoder encoder;
-  //  private final SparkMaxPIDController pid;
-  //  private final CANSparkMax miniVader;
+    private final TalonFX chomp;
+  // Note: These gains are fake, and will have to be tuned for your robot.
+    private final SimpleMotorFeedforward m_feedforward = new SimpleMotorFeedforward(1, 1.5);
+    private final PIDController chompinPID = new PIDController(kP, kI, kD);  
+    private final TrapezoidProfile.Constraints m_constraints =    
+    new TrapezoidProfile.Constraints(1.75, 0.75);
+    private TrapezoidProfile.State m_goal = new TrapezoidProfile.State( );
+    private TrapezoidProfile.State m_setpoint = new TrapezoidProfile.State();
 
+        /**
+     * In order to use PID functionality for a controller, a SparkMaxPIDController object
+     * is constructed by calling the getPIDController() method on an existing
+     * CANSparkMax object
+     */
+        
 
     public IntakeIOReal() {
        // leftSide = new CANSparkMax(IntakeConstants.intakeLeft, MotorType.kBrushless);
        // rightSide = new CANSparkMax(IntakeConstants.intakeRight, MotorType.kBrushless);
       //  miniVader = new CANSparkMax(IntakeConstants.miniVader, MotorType.kBrushless);
-        chomp = new VictorSPX(IntakeConstants.intakeChomp);
+        chomp = new TalonFX(IntakeConstants.intakeChomp);
         counter = new Counter(Counter.Mode.kTwoPulse);
         counter.setUpSource(1);
         counter.setDownSource(2);
-    
         // Set the decoding type to 2X
         counter.setUpSourceEdge(true, true);
         counter.setDownSourceEdge(true, true);
@@ -57,6 +78,28 @@ public class IntakeIOReal implements IntakeIO {
 
        // leftSide.burnFlash();
        // rightSide.burnFlash();
+
+       if (m_joystick.getRawButtonPressed(2)) {
+        m_goal = new TrapezoidProfile.State(5, 0);
+      } else if (m_joystick.getRawButtonPressed(3)) {
+        m_goal = new TrapezoidProfile.State(0, 0);
+      }
+  
+      // Create a motion profile with the given maximum velocity and maximum
+      // acceleration constraints for the next setpoint, the desired goal, and the
+      // current setpoint.
+      var profile = new TrapezoidProfile(m_constraints, m_goal, m_setpoint);
+  
+      // Retrieve the profiled setpoint for the next timestep. This setpoint moves
+      // toward the goal while obeying the constraints.
+      m_setpoint = profile.calculate(kDt);
+  
+          // Send setpoint to offboard controller PID
+      chomp.setSetpoint(
+      chompinPID.
+      m_setpoint.position,
+      m_feedforward.calculate(m_setpoint.velocity) / 12.0);
+
     }
 
   private void enableVoltageCompensation() {
@@ -105,7 +148,6 @@ public void close()
 
 
 
-
 public void setVelocity(double velocityRadPerSec, double ffVolts) {
   //  pid.setReference(
   //      Units.radiansPerSecondToRotationsPerMinute(velocityRadPerSec)
@@ -122,10 +164,7 @@ public void setVelocity(double velocityRadPerSec, double ffVolts) {
   }
 
   public void configurePID(double kP, double kI, double kD) {
-  //  pid.setP(kP, 0);
-  //  pid.setI(kI, 0);
-  //  pid.setD(kD, 0);
-  //  pid.setFF(0, 0);
+    
   }
 
 }
